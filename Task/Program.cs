ba66b30c73efbf904a17f11759df8f3176648241
Adil <christie_m@mail.ru>
2022-09-18 12:12:49 +0300

    program

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+
+Console.Clear();
+
+string[] array = { "Rus", "QA", "number", "163", "Hello world!", "qwe" };
+string[] result = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) result[i] = array[i];
+    else result[i] = string.Empty;
+    if (result[i] != string.Empty) Console.Write($"{result[i]} ");
+}
